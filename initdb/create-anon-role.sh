74bf5c2465f092a315ee93591b82597389655a97
boudewijn overvliet <boudewijn@hello-data.nl>
2023-05-12 21:42:42 +0200

    wip

--- initdb/create-anon-role.sh
+++ initdb/create-anon-role.sh
@@ -14,12 +14,5 @@ psql -U ${POSTGRES_USER} <<-END
     create role todo_user nologin;
     grant todo_user to ${POSTGRES_USER};
 
-    grant usage, select on schema ${DB_SCHEMA} to todo_user;
-    alter default privileges in schema ${DB_SCHEMA} grant insert on tables to todo_user;
-    alter default privileges in schema ${DB_SCHEMA} grant update on tables to todo_user;
-    alter default privileges in schema ${DB_SCHEMA} grant delete on tables to todo_user;
-    alter default privileges grant usage, select on sequences to todo_user;
-    grant usage, select on sequence houses_id_seq to todo_user;
-    grant usage, select on all tables in schema ${DB_SCHEMA} to todo_user;
-    grant usage, select on sequences to todo_user;
+    GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO todo_user;
 END
