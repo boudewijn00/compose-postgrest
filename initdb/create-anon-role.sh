6f3a08d195b0c342829ff2e027dbab4f58273eaf
boudewijn overvliet <boudewijn@hello-data.nl>
2023-05-12 22:07:41 +0200

    wip

--- initdb/create-anon-role.sh
+++ initdb/create-anon-role.sh
@@ -11,7 +11,7 @@ psql -U ${POSTGRES_USER} <<-END
     grant select on all sequences in schema ${DB_SCHEMA} to ${DB_ANON_ROLE};
     grant select on all tables in schema ${DB_SCHEMA} to ${DB_ANON_ROLE};
    
-    create role todo_user;
+    create role todo_user nologin;
     GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO todo_user;
     GRANT ALL PRIVILEGES ON ALL SEQUENCES IN SCHEMA public TO todo_user;
     GRANT DELETE, INSERT, SELECT, UPDATE ON houses IN SCHEMA public TO todo_user;
