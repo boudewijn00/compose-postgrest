260ffc28f6bbce8cfba54893279e3f78dd0493bc
Boudewijn Overvliet <boudewijnovervliet@BoudewiacStudio.fritz.box>
2023-12-29 21:00:13 +0100

    add jobs table

---
+++ schema/revert/create_jobs_table.sql
@@ -0,0 +1,7 @@
+-- Revert postgrest:create_jobs_table from pg
+
+BEGIN;
+
+-- XXX Add DDLs here.
+
+COMMIT;
---
+++ schema/verify/create_jobs_table.sql
@@ -0,0 +1,7 @@
+-- Verify postgrest:create_jobs_table on pg
+
+BEGIN;
+
+-- XXX Add verifications here.
+
+ROLLBACK;
