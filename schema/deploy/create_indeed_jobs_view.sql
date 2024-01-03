daa916cd653e229873abdf11fc9c2ca5fe2ee02a
Boudewijn Overvliet <boudewijnovervliet@BoudewiacStudio.fritz.box>
2024-01-03 19:59:48 +0100

    wip

--- schema/deploy/create_indeed_jobs_view.sql
+++ schema/deploy/create_indeed_jobs_view.sql
@@ -4,18 +4,21 @@ BEGIN;
 
 CREATE VIEW indeed_jobs AS
 select 
-country, 
+job_key,
 to_timestamp(created_date)::date as created_date, 
+country, 
 location, 
-title, 
+company,
 search_term, 
+title,
+snipper,
 salary_type, 
 max_salary_yearly, 
 min_salary_yearly, 
-salary_max, 
-salary_min, 
 salary_currency,
-remote_work_model_type
+remote_work_model_type,
+more_loc_url,
+apply_count
 from indeed 
 where date(inserted_date) = CURRENT_DATE and to_timestamp(created_date)::date > '2023-01-01'
 order by max_salary_yearly desc;
